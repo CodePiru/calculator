df11eeb6acac37096379170a4071b307a81e5c08
CodePiru <coding.jose@gmail.com>
2022-09-06 15:15:33 -0500

    Add Keyboard events to change opacity of btns

--- script.js
+++ script.js
@@ -30,85 +30,98 @@ function divide(a, b) {
 
 function operate(operator, a, b) {
 	switch (operator) {
-		case 'add': 
-		 return add(a, b)
+		case 'add':
+			return add(a, b)
 		case 'subtract':
-		 return subtract(a, b);
+			return subtract(a, b);
 		case 'multiply':
-		 return multiply(a, b);
+			return multiply(a, b);
 		case 'divide':
-		 return divide(a, b);
+			return divide(a, b);
 		default:
-		 throw 'No operator selected';
+			throw 'No operator selected';
 	}
 }
 
-function changeOpacity() {
- this.classList.add('active');
- setTimeout(() => this.classList.remove('active'), 100);
+function addActiveClass() {
+	this.classList.add('active');
+}
+
+function removeActiveClass(e) {
+	if (this.classList.contains('active')) {
+		this.classList.remove('active');
+	}
 }
 
 function displayNumbers() {
- if (isNewNum) displayValue = '';
- if (isError || typeof displayValue != 'string') reset();
- if (displayValue.length < 8) {
-  isNewNum = false;
-  const addedNum = (this.dataset.value == '.' 
-   && displayValue.includes('.')) ? '' : this.dataset.value;
-  displayValue += addedNum;
-  display.textContent = displayValue;
- }
+	if (isNewNum) displayValue = '';
+	if (isError || typeof displayValue != 'string') reset();
+	if (displayValue.length < 8) {
+		isNewNum = false;
+		const addedNum = (this.dataset.value == '.'
+			&& displayValue.includes('.')) ? '' : this.dataset.value;
+		displayValue += addedNum;
+		display.textContent = displayValue;
+	}
 }
 
 function storeNumAndOp() {
- if (isError) reset();
- if (num1 && displayValue) showResult();
- num1 = displayValue;
- operator = this.id;
- isNewNum = true;
+	if (isError) reset();
+	if (num1 && displayValue) showResult();
+	num1 = displayValue;
+	operator = this.id;
+	isNewNum = true;
 }
 
 function showResult() {
- if (isError) reset();
- if (!operator || isNewNum) return;
- if (operator == 'divide' && displayValue == 0) {
-  display.textContent = '#DIV/0';
-  isError = true;
-  return;
- }
- displayValue = Math.round((operate(operator, num1, displayValue) + Number.EPSILON) * 100) / 100;
- if (String(displayValue).length > 11) {
-  display.textContent = '#OVERFLOW';
-  isError = true;
-  return;
- }
- display.textContent = displayValue;
- operator = null;
+	if (isError) reset();
+	if (!operator || isNewNum) return;
+	if (operator == 'divide' && displayValue == 0) {
+		display.textContent = '#DIV/0';
+		isError = true;
+		return;
+	}
+	displayValue = Math.round((operate(operator, num1, displayValue) + Number.EPSILON) * 100) / 100;
+	if (String(displayValue).length > 11) {
+		display.textContent = '#OVERFLOW';
+		isError = true;
+		return;
+	}
+	display.textContent = displayValue;
+	operator = null;
 }
 
 function reset() {
- display.textContent = '';
- num1 = null;
- operator = null;
- displayValue = '';
- isNewNum = false;
- isError = false;
+	display.textContent = '';
+	num1 = null;
+	operator = null;
+	displayValue = '';
+	isNewNum = false;
+	isError = false;
 }
 
 function clearLastDigit() {
- if (isError) reset();
- if (typeof displayValue != 'string') return;
- displayValue = displayValue.slice(0, -1);
- display.textContent = displayValue;
+	if (isError) reset();
+	if (typeof displayValue != 'string') return;
+	displayValue = displayValue.slice(0, -1);
+	display.textContent = displayValue;
 }
 
 function multiplyMinusOne() {
- if (typeof displayValue != 'string') return;
- displayValue = displayValue ? String(displayValue * -1) : '-';
-  display.textContent = displayValue;
+	if (typeof displayValue != 'string') return;
+	displayValue = displayValue ? String(displayValue * -1) : '-';
+	display.textContent = displayValue;
 }
 
-btns.forEach(btn => btn.addEventListener('click', changeOpacity));
+function addActiveClassKey(e) {
+	const btn = document.querySelector(`.btn[data-key="${e.keyCode}"]`);
+	if (!btn) return;
+	btn.classList.add('active');
+}
+
+btns.forEach(btn => btn.addEventListener('click', addActiveClass));
+
+btns.forEach(btn => btn.addEventListener('transitionend', removeActiveClass));
 
 numbers.forEach(number => number.addEventListener('click', displayNumbers));
 
@@ -121,3 +134,5 @@ clear.addEventListener('click', reset);
 backspace.addEventListener('click', clearLastDigit);
 
 negative.addEventListener('click', multiplyMinusOne);
+
+window.addEventListener('keydown', addActiveClassKey)
